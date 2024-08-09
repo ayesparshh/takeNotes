67ec42c2fa2e8ad73d961d7acc7a872a35491571
ayesparshh <sparshgupta1001@gmail.com>
2024-08-09 16:46:54 +0530

    added sessions

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -11,12 +11,11 @@ import (
 	"github.com/go-playground/form/v4"
 )
 
-// Create an newTemplateData() helper, which returns a pointer to a templateData
-// struct initialized with the current year. Note that we're not using the
-// *http.Request parameter here at the moment, but we will do later in the book.
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
 		CurrentYear: time.Now().Year(),
+		// Add the flash message to the template data, if one exists.
+		Flash: app.sessionManager.PopString(r.Context(), "flash"),
 	}
 }
 
