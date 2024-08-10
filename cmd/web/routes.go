9b9c3d3694355a463a63878759f0aa89ee749165
ayesparshh <sparshgupta1001@gmail.com>
2024-08-10 23:58:05 +0530

    added testing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,13 +3,16 @@ package main
 import (
 	"net/http"
 
+	"github.com/ayesparshh/ui"
 	"github.com/julienschmidt/httprouter" // New import
 	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
+
 	// Initialize the router.
 	router := httprouter.New()
+
 	// Create a handler function which wraps our notFound() helper, and then
 	// assign it as the custom handler for 404 Not Found responses. You can also
 	// set a custom handler for 405 Method Not Allowed responses by setting
@@ -18,8 +21,21 @@ func (app *application) routes() http.Handler {
 		app.notFound(w)
 	})
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	// Take the ui.Files embedded filesystem and convert it to a http.FS type so
+	// that it satisfies the http.FileSystem interface. We then pass that to the
+	// http.FileServer() function to create the file server handler.
+	fileServer := http.FileServer(http.FS(ui.Files))
+
+	// Our static files are contained in the "static" folder of the ui.Files
+	// embedded filesystem. So, for example, our CSS stylesheet is located at
+	// "static/css/main.css". This means that we now longer need to strip the
+	// prefix from the request URL -- any requests that start with /static/ can
+	// just be passed directly to the file server and the corresponding static
+	// file will be served (so long as it exists).
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
+
+	// Add a new GET /ping route.
+	router.HandlerFunc(http.MethodGet, "/ping", ping)
 
 	// Add the authenticate() middleware to the chain.
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
@@ -36,6 +52,7 @@ func (app *application) routes() http.Handler {
 	router.Handler(http.MethodGet, "/snippet/create", protected.ThenFunc(app.snippetCreate))
 	router.Handler(http.MethodPost, "/snippet/create", protected.ThenFunc(app.snippetCreatePost))
 	router.Handler(http.MethodPost, "/user/logout", protected.ThenFunc(app.userLogoutPost))
+
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	return standard.Then(router)
 
